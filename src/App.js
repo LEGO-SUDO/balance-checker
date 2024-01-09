e969c6607bc359c9a1d0003c038da569d64def34
LEGO-SUDO <nakulksingh3127@gmail.com>
2024-01-09 15:03:54 +0530

    app fix

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import TokenDisplayComponent from './Components/TokenDisplayComponent'
 import { AddToken } from './Components/AddToken'
 import ChainSelector from './Components/ChainSelector'
 
-// Initial token data for display
 const initialTokens = [
-  // Add other initial tokens as needed
+  {
+    chainName: 'Mantle',
+    tokenType: 'Native',
+    tokenAddress: '',
+    rpcUrl: 'https://rpc.mantle.xyz',
+  },
+  {
+    chainName: 'Linea',
+    tokenType: 'Native',
+    tokenAddress: '',
+    rpcUrl: 'https://rpc.linea.build',
+  },
+  {
+    chainName: 'Kroma',
+    tokenType: 'Native',
+    tokenAddress: '',
+    rpcUrl: 'https://api.kroma.network',
+  },
 ]
 
 function App() {
-  // State for storing user's Ethereum address
   const [address, setAddress] = useState('')
-
-  // State for storing tokens
   const [tokens, setTokens] = useState(initialTokens)
-
-  // State for storing the currently selected blockchain chain
   const [selectedChain, setSelectedChain] = useState('All')
-
-  // State to toggle between showing balances and adding a new token/network
   const [showBalance, setShowBalance] = useState(true)
 
-  // Load tokens from localStorage or initialize with default tokens
   useEffect(() => {
     const storedTokens = localStorage.getItem('tokens')
     if (storedTokens) {
@@ -31,12 +39,9 @@ function App() {
     }
   }, [])
 
-  // Handler for changing the selected blockchain chain
   const handleChainSelection = (chain) => {
     setSelectedChain(chain)
   }
-
-  // Handler for adding a new token
   const handleAddToken = (newTokenData) => {
     const updatedTokens = [...tokens, newTokenData]
     setTokens(updatedTokens)
@@ -45,26 +50,53 @@ function App() {
 
   return (
     <div className='relative w-[100vw] h-[100vh]'>
-      {/* Background and styling */}
+      <div className='absolute w-[30vw] h-[50vh] left-[34%] top-[24%] bg-[#fc72ff] z-0 blur-[100px]' />
       <div className='bg-black absolute left-[30%] top-[20%] min-w-[320px] w-[40vw] h-[60vh] rounded-[2vw] py-[20px] px-[20px] z-50'>
-        {/* Header section for balance and add network */}
         <div className='w-full py-[10px] flex justify-between items-center'>
-          {/* Balance and Add Network toggle buttons */}
+          <div className='flex text-[#FAFAFA] '>
+            <div
+              className={` w-[150px] flex items-center justify-center ${
+                showBalance
+                  ? 'px-[12px] py-[8px] rounded-[20px] bg-[#fc72ff] hover:bg-[#f892fa]'
+                  : 'border-[#202020cc] border-l-[2px] border-y-[2px] px-[8px] py-[8px] rounded-l-[20px] cursor-pointer'
+              } `}
+              onClick={() => setShowBalance(true)}
+            >
+              Balance
+            </div>
+            <div
+              className={`py-[8px]  w-[150px] flex items-center justify-center ${
+                !showBalance
+                  ? 'px-[12px] rounded-[20px] bg-[#fc72ff] hover:bg-[#f892fa]'
+                  : 'border-y-[2px] px-[8px] py-[8px] rounded-r-[20px] border-[#202020cc] border-r-[2px] cursor-pointer'
+              }`}
+              onClick={() => setShowBalance(false)}
+            >
+              Add NetWork
+            </div>
+          </div>
+          <ChainSelector onChainSelect={handleChainSelection} />
         </div>
-
-        {/* Conditionally render TokenDisplayComponent or AddToken based on toggle state */}
-        {showBalance ? (
+        {showBalance && (
           <>
-            {/* User address input field */}
+            {' '}
+            <div className='flex w-full gap-[12px] items-center justify-center'>
+              <input
+                type='text'
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                placeholder='Enter your address 0x...daf'
+                className=' w-full bg-[#202020] py-[10px] px-[12px] rounded-[8px] text-[#808080] outline-none'
+              />
+            </div>
             <TokenDisplayComponent
               userAddress={address}
               tokens={tokens}
               selectedChain={selectedChain}
-            />
+            />{' '}
           </>
-        ) : (
-          <AddToken onAddToken={handleAddToken} />
         )}
+        {!showBalance && <AddToken onAddToken={handleAddToken} />}
       </div>
     </div>
   )
